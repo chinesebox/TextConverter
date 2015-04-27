bc087bff5abedea9a5a04847a12d5f70863231b4
chinesebox <chinesebox@github.com>
2015-04-27 15:23:08 +0200

    Refactorization + model added

--- src/main/java/org/textconverter/Main.java
+++ src/main/java/org/textconverter/Main.java
@@ -1,6 +1,7 @@
 package org.textconverter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +14,8 @@ import java.util.regex.Pattern;
 public class Main {
 
 	private static final String CSV_DELIMITER = ",";
+	
+	private static final String TMP_FILE = "output.txt";
 
 	public static void main(String[] args) throws IOException, Exception {
 		System.out.println("Enter output strategy: ");
@@ -20,15 +23,58 @@ public class Main {
 		System.out.println("[2] CSV: ");
 
 		Scanner strategyTypeScanner = new Scanner(System.in);
-		String in = null;
+		String in = "";
 		while (strategyTypeScanner.hasNext() && ((in = strategyTypeScanner.next()).equals("1") && in.equals("2"))) {
-			System.out.println(in);
 		}
 		
 		System.out.println("Use CTRL + Z to close input");
 		System.out.println("Enter text: ");
 		Scanner inputTextScanner = new Scanner(System.in);
+		
+		int max = write(inputTextScanner);
+		
+		strategyTypeScanner.close();
+		inputTextScanner.close();
 
+		switch (in) {
+		case "1": {
+			printXML();
+			break;
+		}
+		case "2": {
+			printCSV(max);
+			break;
+		}
+		default:
+			throw new Exception("No option valaibe");
+		}
+			
+		System.out.println();
+		System.out.println("END");
+
+	}
+
+	/**
+	 * Initialize PrintWriter
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	private static PrintWriter getPrintWriter() throws IOException {
+		File outFile = new File(TMP_FILE);
+		FileWriter fWriter = new FileWriter(outFile);
+		PrintWriter pWriter = new PrintWriter(fWriter);
+		return pWriter;
+	}
+	
+	/**
+	 * Writing to tmp file
+	 * 
+	 * @param inputTextScanner
+	 * @return
+	 * @throws IOException
+	 */
+	private static int write(Scanner inputTextScanner) throws IOException {
 		inputTextScanner.useDelimiter("\\.");
 		String sentence;
 		int max = 0;
@@ -51,84 +97,64 @@ public class Main {
 		}
 		pw.close();
 		
-		strategyTypeScanner.close();
-		inputTextScanner.close();
-
-		if (null != in) {
-			switch (in) {
-			case "1": {
-				File inFile = new File("output.txt");
-				Scanner sc = new Scanner(inFile);
-				System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
-				System.out.println("<text>");
-				System.out.println("	<sentence>");
-				while (sc.hasNextLine()) {
-					String word = sc.nextLine();
-					if("".equals(word)) {
-						if(sc.hasNextLine()) {
-							System.out.println("	</sentence>");
-							System.out.println("	<sentence>");
-						}
-						continue;
-					}
-					System.out.println("		<word>" + word + "</word>");
+		return max;
+	}
+	
+	/**
+	 * Converting tmp file to XML
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	private static void printXML() throws FileNotFoundException {
+		File inFile = new File(TMP_FILE);
+		Scanner sc = new Scanner(inFile);
+		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+		System.out.println("<text>");
+		System.out.println("	<sentence>");
+		while (sc.hasNextLine()) {
+			String word = sc.nextLine();
+			if("".equals(word)) {
+				if(sc.hasNextLine()) {
+					System.out.println("	</sentence>");
+					System.out.println("	<sentence>");
 				}
-				System.out.println("	</sentence>");
-				System.out.println("</text>");
-				sc.close();
-				break;
+				continue;
 			}
-			case "2": {
-				File inFile = new File("output.txt");
-				Scanner sc = new Scanner(inFile);
-				System.out.println();
-				for(int i = 0; i < max; i++) {
-					System.out.print(CSV_DELIMITER + " Word " + (i + 1));
-				}
-				int sentenceCount = 0;
-				System.out.println();
-				System.out.print("SENTENCE " + ++sentenceCount);
-				while (sc.hasNextLine()) {
-					String word = sc.nextLine();
-					if("".equals(word)) {
-						if(sc.hasNextLine()) {
-							System.out.println();
-							System.out.print("SENTENCE " + ++sentenceCount);
-						}
-						continue;
-					}
-					System.out.print(CSV_DELIMITER + " " + word);
-				}
-				sc.close();
-				break;
-			}
-			default:
-				throw new Exception("No option valaibe");
-			}
+			System.out.println("		<word>" + word + "</word>");
 		}
+		System.out.println("	</sentence>");
+		System.out.println("</text>");
+		sc.close();
+	}
+
+	/**
+	 * Converting tmp file to CSV
+	 * 
+	 * @param max
+	 * @throws FileNotFoundException
+	 */
+	private static void printCSV(int max) throws FileNotFoundException {
+		File inFile = new File(TMP_FILE);
+		Scanner sc = new Scanner(inFile);
 		System.out.println();
-		System.out.println("END");
-
-	}
-
-	private static void write() throws IOException {
-		File outFile = new File("output.txt");
-		FileWriter fWriter = new FileWriter(outFile);
-		PrintWriter pWriter = new PrintWriter(fWriter);
-		pWriter.println("This is a line.");
-		pWriter.println("This is another line.");
-		pWriter.close();
-	}
-
-	private static PrintWriter getPrintWriter() throws IOException {
-		File outFile = new File("output.txt");
-		FileWriter fWriter = new FileWriter(outFile);
-		PrintWriter pWriter = new PrintWriter(fWriter);
-		return pWriter;
-	}
-
-	private static void print() {
-
+		for(int i = 0; i < max; i++) {
+			System.out.print(CSV_DELIMITER + " Word " + (i + 1));
+		}
+		int sentenceCount = 0;
+		System.out.println();
+		System.out.print("SENTENCE " + ++sentenceCount);
+		while (sc.hasNextLine()) {
+			String word = sc.nextLine();
+			if("".equals(word)) {
+				if(sc.hasNextLine()) {
+					System.out.println();
+					System.out.print("SENTENCE " + ++sentenceCount);
+				}
+				continue;
+			}
+			System.out.print(CSV_DELIMITER + " " + word);
+		}
+		sc.close();
 	}
 
 }
---
+++ src/main/java/org/textconverter/Text.java
@@ -0,0 +1,49 @@
+package org.textconverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Text contains list of sentences
+ * 
+ * @author ocean
+ */
+public class Text {
+	
+	private List<Sentence> sentences = new ArrayList<Sentence>();
+
+	public List<Sentence> getSentences() {
+		return sentences;
+	}
+
+	public void setSentences(List<Sentence> sentences) {
+		this.sentences = sentences;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((sentences == null) ? 0 : sentences.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Text other = (Text) obj;
+		if (sentences == null) {
+			if (other.sentences != null)
+				return false;
+		} else if (!sentences.equals(other.sentences))
+			return false;
+		return true;
+	}
+	
+}
